653a5e5bd68d2a735e04877e14706168a508ec86
yannick2009 <yannickkouakou13@gmail.com>
2024-05-04 18:22:03 +0200

    chores: rebranding of Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,27 +1,38 @@
 import "./styles/Login.scss";
 import logoImg from "@assets/logo.svg";
+import loginIllustrationImg from "@assets/login-illustration.png";
 import googleLogoImg from "@assets/logo-google.png";
 import { login } from "@api/index";
 
-export default function Login() {
+export default function Login(): JSX.Element {
   const handleLogin = async () => {
     login();
   };
 
   return (
     <main id="login">
-      <div id="login__field">
-        <img src={logoImg} alt="Critik-V logo" />
-        <h2>Se connecter à son compte</h2>
-        <p>
-          Bienvenue sur Critik-V, une application de review de CV anonyme par d'autres utilisateurs.
-          Obtenez des commentaires constructifs pour améliorer votre CV.
-        </p>
-        <button onClick={handleLogin}>
-          <img src={googleLogoImg} alt="Google logo" />
-          Se connecter avec Google
-        </button>
-      </div>
+      <section className="logo">
+        <img src={logoImg} alt="logo" />
+      </section>
+      <section className="container">
+        <div className="left">
+          <img src={loginIllustrationImg} alt="login illustration" />
+        </div>
+        <div className="right">
+          <h1>
+            Laissez votre CV se faire critiquer, ouvrez la porte à de nouvelles opportunités !
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque vero voluptatum
+            beatae nobis neque doloribus veritatis debitis eius inventore magni aut vel corporis
+            quidem voluptatem adipisci, error eligendi assumenda dolor?
+          </p>
+          <button onClick={handleLogin}>
+            <img src={googleLogoImg} alt="Google logo" />
+            Se connecter avec Google
+          </button>
+        </div>
+      </section>
     </main>
   );
 }
